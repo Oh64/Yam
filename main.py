f7e811964822a93f6f31973aa3a3450d275c2450
Oh64 <93646709+Oh64@users.noreply.github.com>
2024-10-14 15:49:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,3 @@ for i in range(len(rdes)):
     des[(rdes[i-1]-1)] = randint(1,6)
 desachanger = str(input(f"Voici tes 5 dés : \n - Dés 1 : {des[0]} \n - Dés 2 : {des[1]} \n - Dés 3 : {des[2]} \n - Dés 4 : {des[3]} \n - Dés 5 : {des[4]}  \n Voulez vous en relancer ({relance} restant) ? \n"))
 
-
-hifzoaof
